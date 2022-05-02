504b74edf476526f6162b1e4e59e891277e56ba8
gshah1999 <100558905+gshah1999@users.noreply.github.com>
2022-05-02 14:26:45 -0700

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,10 @@
+#the code below will allow us to create TwitterBot
+#However, the main codes are in twitterbot.py file
+#this .py file will communicate with twitterbot.py file which has main code
+# this file only has information about your api keys, access token keys and so _json
+
+API_KEY = 'IHxj70CY0Wm8XG351i65qu2l3'
+API_SECRET_KEY = 'eme99pLmZXaO6rUfNSSh36JqSPM4h7TeIFrv0DwvCp6QZ39Wka'
+ACCESS_TOKEN = '1517551738684727302-pkrYmyfefCNfiGKehs7WvRnt0eMCz8'
+SECRET_ACCESS_TOKEN = 'xLkdiNZQwFIHB5L70sWKbZT13CDgBWOVbZ7yiPp4fYDsO'
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAHYfbwEAAAAAPRhP8vkPukOxa31pJDCkshMT8cw%3DU3jXGWuIulIStkJAn6wMM1fFaHG1atLrcqoeQX9Tmfzb76gbVo'
